08bbac39fbdf8bf5d7132d4dcfe10dc69271d75b
Saber Ben Rejeb <rejebsaber@gmail.com>
2021-10-11 21:05:47 -0400

    add aws amplify components as storage & api & function

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -24,6 +24,20 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
+    ],
+    "aws_dynamodb_all_tables_region": "us-east-1",
+    "aws_dynamodb_table_schemas": [
+        {
+            "tableName": "organizations-dev",
+            "region": "us-east-1"
+        }
+    ],
+    "aws_cloud_logic_custom": [
+        {
+            "name": "organizations",
+            "endpoint": "https://whkui8zal0.execute-api.us-east-1.amazonaws.com/dev",
+            "region": "us-east-1"
+        }
     ]
 };
 
